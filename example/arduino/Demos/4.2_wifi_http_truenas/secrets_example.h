cf16ceba0021c218ad3e094d964ac5de8db8ebd1
Taikun <taikun@Taikuns-MacBook-Air.local>
2025-06-22 19:51:43 +0200

    Secrets help in english

--- example/arduino/Demos/4.2_wifi_http_truenas/secrets_example.h
+++ example/arduino/Demos/4.2_wifi_http_truenas/secrets_example.h
@@ -1,4 +1,4 @@
-#define WIFI_SSID "TuSSID"
-#define WIFI_PASSWORD "TuClaveWiFi"
+#define WIFI_SSID "YourSSID"
+#define WIFI_PASSWORD "YourWIFIPass"
 #define TRUENAS_URL "https://192.168.X.X/api/v2.0/"
-#define TRUENAS_API_KEY "TU_API_KEY_AQUI"
+#define TRUENAS_API_KEY "YOURAPIKEYHERE"
